392360f1711ffb3d0b1a44f060cc6edb01ab84cf
AmansWork-24-28 <amansoffice25@gmail.com>
2026-07-28 19:36:57 +0530

    Added next

---
+++ 01.Core_Java/25 this&super.java
@@ -0,0 +1,31 @@
+class Animal {
+
+    String color = "White";
+
+    Animal() {
+        System.out.println("Animal Constructor");
+    }
+}
+
+class Dog extends Animal {
+
+    String color = "Black";
+
+    Dog() {
+        super();    // Calls parent class constructor
+        System.out.println("Dog Constructor");
+    }
+
+    void display() {
+        System.out.println("Current Class Color : " + this.color);
+        System.out.println("Parent Class Color  : " + super.color);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        Dog d = new Dog();
+        d.display();
+    }
+}
