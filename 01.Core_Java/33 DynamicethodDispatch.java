648671a7d265409590d02f0318b51e09dc404de9
AmansWork-24-28 <amansoffice25@gmail.com>
2026-07-30 20:19:49 +0530

    Added next

---
+++ 01.Core_Java/33 DynamicethodDispatch.java
@@ -0,0 +1,32 @@
+class A{
+    public void show(){
+        System.out.println("I am A");
+    }
+}
+
+class B extends A{
+    public void show(){
+        System.out.println("I am B");
+    }
+}
+
+class C extends A{
+    public void show(){
+        System.out.println("I am C");
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Dynamic method dispatch ");
+        
+        A oj = new A();
+        oj.show();
+        
+        oj = new B();
+        oj.show();
+        
+        oj = new C();
+        oj.show();
+    }
+}
