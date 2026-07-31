dec438fcf7208168b639448bd246e4abc4267476
AmansWork-24-28 <amansoffice25@gmail.com>
2026-07-31 20:03:47 +0530

    Added next

---
+++ 01.Core_Java/36 AbstractKeyword.java
@@ -0,0 +1,24 @@
+abstract class Car{ // abstract keyword
+    public void race(){
+        System.out.println("Fast and Furious");
+    }
+    
+    public abstract void horn(); // abstract function
+}
+
+class Gwagon extends Car{
+    public void horn(){
+        System.out.println("forceee-->");
+    }
+}
+
+
+class AbstractKeyword{ {
+    public static void main(String[] args) {
+        System.out.println("Abstract keyword");
+        
+        Gwagon g = new Gwagon();
+        g.race();
+        g.horn();
+    }
+}
