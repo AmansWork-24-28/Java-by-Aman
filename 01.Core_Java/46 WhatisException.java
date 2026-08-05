7777dd3dbcab3deeda768438703b65090de9392c
AmansWork-24-28 <amansoffice25@gmail.com>
2026-08-05 23:27:05 +0530

    Add DS&ML practical 1

---
+++ 01.Core_Java/46 WhatisException.java
@@ -0,0 +1,15 @@
+//         Types of error:-
+// 1.      Complile - time error
+// 2.      Runtime error -> Exception handling
+// 3.      Logical error
+
+class WhatisException {
+    public static void main(String[] args) {
+        System.out.println("What is Exception");
+        
+        int i = 0;
+        int j = 10/i;
+         System.out.print("Error occured ... Execution is stopped");
+    
+    }
+}
